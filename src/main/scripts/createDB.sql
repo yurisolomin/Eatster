31c2e6b69515ae30fdf51d6354233a8a2f449626
yurisolomin <yurisolomin@mail.ru>
2016-02-02 16:35:54 +0300

    import

--- src/main/scripts/createDB.sql
+++ src/main/scripts/createDB.sql
@@ -1,4 +1,4 @@
-create role eatster_user login password 'eat' nosuperuser;
+create role eatster_user login password '***' nosuperuser;
 create database eatster with owner=eatster_user encoding='UTF8';
 
 
